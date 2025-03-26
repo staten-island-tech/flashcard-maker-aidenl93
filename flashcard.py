e825035df9a016300c28e74e10737a4dd8d07ffa
aidenl93 <aidenl93@nycstudents.net>
2025-03-26 09:52:37 -0400

    Update flashcard.py

---
+++ flashcard.py
@@ -0,0 +1,8 @@
+class user:
+    def __init__(self,name,email):
+        self.name = name 
+        self.email = email 
+
+    def displayinfo(self):
+        return f"user: {self.name}, {self.email}"
+
