5bdd5fbae745cce0247a33e29b98fa44741e5e23
aidenl93 <aidenl93@nycstudents.net>
2025-04-03 09:34:34 -0400

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -1,11 +1,20 @@
 import json 
 class teach:
     def __init__(self):
-        self.store = {}
-    def addpair(self, key, value):
-        self.store[key] = value 
-    def getvalue(self,key):
-        return self.store.get(self, key)
-    def display(self):
-        for key, value in self.store.items():
-            print(f"ok {key} {value}")
+        self.ok = {}
+    def adding(self):
+        while True:
+            make = input("give key (or quit by saying quit)")
+            if make.lower() == "quit":
+                break
+            make2 = input("give value")
+            self.ok[make] = make2 
+    def show(self):
+        print(self.ok)
+
+yaw = input("1 for teacher 2 for study")
+if yaw == "1":
+    bro = teach()
+    bro.adding()
+    bro.show()
+        
